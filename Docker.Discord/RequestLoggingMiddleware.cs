e3a81403929c4065335d77f0d6084ef83d419b4a
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-09-23 04:06:28 -0400

    Log headers for debug purposes

--- Docker.Discord/RequestLoggingMiddleware.cs
+++ Docker.Discord/RequestLoggingMiddleware.cs
@@ -18,7 +18,7 @@ namespace Docker.Discord
 
 		public async Task Invoke(HttpContext context)
 		{
-			_logger.LogInformation("Request to {Method} {Url}{Query}",context.Request.Method, context.Request.Path, context.Request.QueryString);
+			_logger.LogInformation("Request to {Method} {Url}{Query} | Headers: {Headers}",context.Request.Method, context.Request.Path, context.Request.QueryString, @context.Request.Headers);
 			await _next.Invoke(context);
 			
 		}
