8bd48a167c9b69dae04a2c376ac46e3782af984f
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-11-09 09:38:26 -0500

    TODO: Figure out Docker via C#

---
+++ Docker.Discord/Services/DockerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace Docker.Discord.Services;
+
+public class DockerService
+{
+	private readonly DockerClient _dockerClient;
+
+	public DockerService()
+    {
+	    var dockerEndpoint = "unix:/var/run/docker.sock";
+
+	    if (OperatingSystem.IsWindows())
+		    dockerEndpoint = "npipe://./pipe/docker_engine";
+	    
+	    _dockerClient = new DockerClientConfiguration(new Uri(dockerEndpoint)).CreateClient();
+    }
+
+	public Task RunImageAsync(string image, string tag = "latest")
+		=> _dockerClient.Images.CreateImageAsync(new()
+		{
+			FromImage = image,
+            Tag = tag
+        }, new(), new Progress<JSONMessage>());
+
+
+
+}
